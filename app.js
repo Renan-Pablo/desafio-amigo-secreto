d49cdcb5587fae2ab769a5f84437542c80d3fa9f
Renan Pablo <kenanpablo123@gmail.com>
2025-07-26 13:43:10 -0300

    codigo-completo

---
+++ app.js
@@ -0,0 +1,44 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById('nome-amigo');
+    const nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+   
+    amigos.push(nome);
+    input.value = "";
+
+
+
+    atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""; // Limpa a lista
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um amigo para sortear.");
+        return;
+    }
+
+    const indice = Math.floor(Math.random() * amigos.length);
+
+    const nomeSorteado = amigos[indice];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>${nomeSorteado} foi sorteado!</li>`;
+}
